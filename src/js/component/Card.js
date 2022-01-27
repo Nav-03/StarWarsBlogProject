efcfb11d6f0405d1b015338d8eac2a9be5c4e830
Nav-03 <navil.gabrie@gmail.com>
2022-01-27 23:37:01 +0000

    minor update

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -9,14 +9,14 @@ const Card = (props) => {
   let buttonClass = "btn btn-outline-danger";
   if(props.isFavorite) buttonClass = "btn btn-danger";
 
-  console.log("the card for "+entity.name+" has the class "+buttonClass, props.isFavorite);
+  // console.log("the card for "+entity.name+" has the class "+buttonClass, props.isFavorite);
 
   return (
     <div className="card mx-4" style={{ width: "20rem", display: "inline-block" }}>
       <img className="card-img-top" width="200px" height="300px" src={store.images[entity.type + entity.uid] || "https://snr.unl.edu/images/portrait-large/staff/whiteblank.gif"} alt="Card image cap" />
       <div className="card-body">
         <h5 className="card-title">{entity.name}</h5>
-        <p className="card-text text-wrap">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p className="card-text text-wrap">{entity.url}</p>
         <div className="d-flex justify-content-around align-items-center">
         <Link to={`${entity.type}/${entity.uid}`}>
           <a href="#" className="btn btn-primary ">Learn More!</a>
